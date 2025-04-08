2c9fb3886e97a9794be35e860dd297ea941a0917
felipesantos2 <santospinheiro627@gmail.com>
2025-04-08 19:55:02 -0300

    feat: update miration and add nre js libraries

--- database/migrations/2025_04_08_182538_create_posts_table.php
+++ database/migrations/2025_04_08_182538_create_posts_table.php
@@ -4,13 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('content');
+            $table->string('title')->nullable();
+            $table->text('content')->nullable();
             $table->string('slug');
             $table->string('thumbnail')->nullable();
             $table->string('post_type')->nullable();
